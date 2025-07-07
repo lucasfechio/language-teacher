1dcdd2bd54a9aa945ab2513085b0b5966933aa50
lucasfechio <lucasfechio94@gmail.com>
2025-07-07 19:54:20 -0300

    feat: add messages box component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import MessageBox from './components/MessagesBox'
+
 function App() {
   return (
-    <div className="flex justify-center w-full h-screen items-center">
-      Language Teacher
+    <div className="flex justify-center w-full h-screen items-center p-[50px]">
+      <MessageBox />
     </div>
   )
 }
